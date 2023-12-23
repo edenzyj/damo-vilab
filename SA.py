6c51eed5d776821789fe68272460f8decbae6358
zyj <edenzyj88@gmail.com>
2023-12-23 18:53:15 +0800

    add iottalk related code

---
+++ SA.py
@@ -0,0 +1,35 @@
+import gen_test
+import base64
+
+ServerURL = 'https://class.iottalk.tw' #For example: 'https://iottalk.tw'
+MQTT_broker = 'class.iottalk.tw' # MQTT Broker address, for example:  'iottalk.tw' or None = no MQTT support
+MQTT_port = 5566
+MQTT_encryption = True
+MQTT_User = 'iottalk'
+MQTT_PW = 'iottalk2023'
+
+device_model = 'Music_to_video'
+IDF_list = ['MP4_I']
+ODF_list = ['Sentence_O']
+device_id = '31283301712230428 ' #if None, device_id = MAC address
+device_name = 'eden_device'
+exec_interval = 1  # IDF/ODF interval
+
+def on_register(r):
+    print('Server: {}\nDevice name: {}\nRegister successfully.'.format(r['server'], r['d_name']))
+
+have_recieved = [0]
+filename = '/home/zyj/Module-class/damo-vilab/output/test.mp4'
+
+def MP4_I():
+    if have_recieved[0] == 0: return None
+    have_recieved[0] = 0
+    mp4_file = open(filename, 'rb')
+    mp4_binary_data = mp4_file.read()
+    base64_encoded = base64.b64encode(mp4_binary_data)
+    base64_string = base64_encoded.decode('utf-8')
+    return base64_string
+
+def Sentence_O(data:list):
+    have_recieved[0] = 1
+    gen_test.gen_video(data)
---
+++ gen_test.py
@@ -0,0 +1,15 @@
+import torch, gc
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers.utils import export_to_video
+
+def gen_video(prompt):
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    pipe = DiffusionPipeline.from_pretrained("text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    pipe.enable_model_cpu_offload()
+    pipe.enable_vae_slicing()
+
+    video_frames = pipe(prompt, num_inference_steps=25, num_frames=40).frames
+    video_path = export_to_video(video_frames, output_video_path="/home/zyj/Module-class/damo-vilab/output/long.mp4")
